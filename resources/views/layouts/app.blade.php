ad959a460bdc9833fbf8d32f29d6cb9569a2e0b9
Ибрагим Аушев <aushevibra@yandex.ru>
2019-06-15 16:17:39 +0300

    profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,10 +60,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('profile') }}">
+                                        {{ __('Профиль') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Выйти') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
